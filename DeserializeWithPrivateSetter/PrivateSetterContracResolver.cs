2fcf3bb407971a2c2308a44ea0b3b3b2a38cdf23
ndq3004 <ndq3004@gmail.com>
2025-06-14 17:02:03 +0700

    Add project files.

---
+++ DeserializeWithPrivateSetter/PrivateSetterContracResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace DeserializeWithPrivateSetter
+{
+    public class PrivateSetterContracResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            JsonProperty prop = base.CreateProperty(member, memberSerialization);
+
+            if (!prop.Writable)
+            {
+                var property = member as PropertyInfo;
+                if (property != null)
+                {
+                    // Check if the setter is non-public but exists
+                    var setter = property.GetSetMethod(true);
+                    if (setter != null)
+                    {
+                        prop.Writable = true;
+                    }
+                }
+            }
+
+            return prop;
+        }
+    }
+}
---
+++ DeserializeWithPrivateSetter/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DeserializeWithPrivateSetter;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+string projectJSON = @"{
+    'Id': 1,
+    'Name': 'Project-ATA',
+    'Description': 'Produce pure energy',
+    'Price': 10000
+}";
+
+var settings = new JsonSerializerSettings
+{
+    ContractResolver = new PrivateSetterContracResolver()
+};
+
+var deserialized = JsonConvert.DeserializeObject<Project>(projectJSON, settings);
+
+if (deserialized != null )
+    Console.WriteLine("Price of the Project: {0}", deserialized.Price);
+
+Console.ReadLine();
